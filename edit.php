990d0151049030f1995e96d2d2bde669bbc722e0
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-09 09:07:46 +0100

    ajout partials

---
+++ edit.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once './view/edit.view.php';
---
+++ view/ajout.view.php
@@ -0,0 +1,13 @@
+<?php
+require_once './inc/fonctions.php';
+require_once './inc/pdo.php';
+require_once './partials/head.php'
+
+?>
+
+
+
+
+<?php
+
+require_once './partials/foot.php';
